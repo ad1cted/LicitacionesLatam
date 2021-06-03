0f3bfbe310752f5419010b30e20ef59ba1cf3b14
Esteban Plaza <esteban.plaza.a@usach.cl>
2021-06-02 23:31:04 -0400

    CHG: Licitacion View

--- api/views/LicitacionViews.py
+++ api/views/LicitacionViews.py
@@ -1,29 +1,18 @@
 import json
 
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.handlers.wsgi import WSGIRequest
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from kernel.models import Moneda, Licitacion, Pais
+from kernel.models import Moneda, Licitacion, Pais, Organismo, Ejecutivo
 from kernel.serializers import LicitacionSerializer
 
 
-def get_licitacion(request: WSGIRequest, arg: str = None, isocode: str = None):
-    if isocode:
-        try:
-            print(Pais.objects.all().count())
-            pais = Pais.objects.filter(nombre__exact='Mexico')
-            print(pais)
-        except ObjectDoesNotExist:
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
+@api_view(['GET'])
+def get_licitacion(request: WSGIRequest, arg: str = None):
     if arg:
-
-        try:
-            licitacion: list = [Licitacion.objects.get(id=arg)]
-        except ObjectDoesNotExist:
-            licitacion = []
+        licitacion: list = [Licitacion.objects.get(id=arg)]
     else:
         licitacion: list = Licitacion.objects.all()
     serializer: LicitacionSerializer = LicitacionSerializer(licitacion, many=True)
@@ -33,29 +22,45 @@ def get_licitacion(request: WSGIRequest, arg: str = None, isocode: str = None):
 @api_view(['POST'])
 def create_licitacion(request: WSGIRequest) -> Response:
     body: dict = json.loads(request.body)
-    external_id: str = body.get("external_id", None)
+
+    external_id: int = body.get("external_id", None)
     raw_data: str = body.get("raw_data", None)
-    nombre: str = body.get("nombre", None)
-    fecha_publicacion: str = body.get("fecha_publicacion", None)
-    fecha_apertura: str = body.get("fecha_apertura", None)
-    cod_country: str = body.get("cod_country", None)
-    origen: str = body.get("origen", None)
-    isocode: str = body.get("isocode", None)
-    id_licitante: str = body.get("id_licitante", None)
-    id_estado_licitacion: str = body.get("id_estado_licitacion", None)
-    pushed: bool = body.get("pushed", None)
+    titulo: str = body.get("titulo", None)
+    fecha_licitacion: str = body.get("fecha_licitacion", None)
+    importe_contrato: str = body.get("importe_contrato", None)
+    try:
+        isocode_moneda: str = body.get("isocode_moneda", None)
+        moneda = Moneda.objects.get(isocode=isocode_moneda)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": "error con moneda"})
+    try:
+        isocode_pais: str = body.get("isocode_pais", None)
+        pais = Pais.objects.get(isocode=isocode_pais)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": "error con pais"})
+    try:
+        id_ejecutivo: str = body.get("id_Ejecutivo", None)
+        ejecutivo = Ejecutivo.objects.get(id=id_ejecutivo)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": "error con ejecutivo"})
+    try:
+        id_organismo: str = body.get("id_Organismo", None)
+        organismo = Organismo.objects.get(id=id_organismo)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": "error con organismo"})
+
     sended_status = status.HTTP_206_PARTIAL_CONTENT
-    if nombre and isocode and id_estado_licitacion:
+    if titulo:
         Licitacion.objects.create(
-            external_id=external_id,
+            titulo=titulo,
+            fecha_licitacion=fecha_licitacion,
+            importe_contrato=importe_contrato,
+            id_Moneda=moneda,
             raw_data=raw_data,
-            nombre=nombre,
-            fecha_publicacion=fecha_publicacion,
-            fecha_apertura=fecha_apertura,
-            cod_country=cod_country,
-            origen=origen,
-            isocode=Moneda.objects.get(isocode=isocode),
-            pushed=pushed)
+            external_id=external_id,
+            id_Pais=pais,
+            id_Organismo=organismo,
+            id_Ejecutivo=ejecutivo)
 
         sended_status: int = status.HTTP_201_CREATED
     return Response(status=sended_status)
